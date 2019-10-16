9e7ebf4fa9fac5bf1d2290bf6a21b35f0703c76d
jumdtw <ttnmrm1m3k5g1m3r3k@gmail.com>
2019-10-16 10:59:39 +0900

    deketa

--- file_controller.cpp
+++ file_controller.cpp
@@ -3,6 +3,18 @@
 #include<fstream>
 #include<cstdio>
 
+// return_edian_high($4016) -> return 0x40
+char return_edian_high(unsigned int code){
+    code = code >> 8;
+    return (unsigned char)code; 
+}
+
+// return_edian_low($4016) -> return 0x16
+unsigned char return_edian_low(unsigned int code){
+    return (unsigned char)code;
+}
+
+
 std::vector<std::string> read_file(std::string asmtext){
     string asmcode;
     vector<string> asmcodes;
@@ -18,8 +30,35 @@ std::vector<std::string> read_file(std::string asmtext){
 }
 
 void write_file(vector<TOKEN> token_vector,string asmtext){
+    unsigned char low,high;
     ofstream ofs(asmtext, ios::binary);
-    ofs.write(reinterpret_cast<char *>(&token_vector[0].opecode),sizeof(token_vector[0].opecode));
+    const unsigned int file_size_16k = 16384;
+    unsigned int file_size_count = 0; 
+    unsigned char pate = 0xff,zero = 0x00,end_0x80 = 0x80;
+
+    for(int i=0;i<token_vector.size();i++){
+        ofs.write(reinterpret_cast<char *>(&token_vector[i].opecode),sizeof(token_vector[i].opecode));
+        file_size_count += 1;
+        low = return_edian_low(token_vector[i].operand);
+        ofs.write(reinterpret_cast<char *>(&low),sizeof(low));
+        file_size_count += 1;
+        if(token_vector[i].size >=3){
+            high = return_edian_high(token_vector[i].operand);
+            ofs.write(reinterpret_cast<char *>(&high),sizeof(high));
+            file_size_count += 1;
+        }
+    }
+    
+    for(int i=0;i<(file_size_16k-file_size_count-6);i++){
+        ofs.write(reinterpret_cast<char *>(&pate),sizeof(pate));
+    }
+    ofs.write(reinterpret_cast<char *>(&zero),sizeof(zero));
+    ofs.write(reinterpret_cast<char *>(&zero),sizeof(zero));
+    ofs.write(reinterpret_cast<char *>(&zero),sizeof(zero));
+    ofs.write(reinterpret_cast<char *>(&end_0x80),sizeof(end_0x80));
+    ofs.write(reinterpret_cast<char *>(&zero),sizeof(zero));
+    ofs.write(reinterpret_cast<char *>(&zero),sizeof(zero));
+
     if(ofs.bad()) {
         cout << "error" << endl;
     }
